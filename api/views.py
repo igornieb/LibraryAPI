ce0b548237e10426bc613670806b644ed91b2297
igornieb <igornieb@gmail.com>
2023-05-11 11:12:41 +0200

    created BookList view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from core.models import *
+from api.serializers import BookSerializer
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView
+from api.permissions import IsStaff
+
+class BookList(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def create(self, request, *args, **kwargs):
+        pass
 
 # Create your views here.
