0a0b3ce7870069b64a2475bd580e24ad14fd08e2
igornieb <igornieb@gmail.com>
2023-05-11 14:04:05 +0200

    added urls for auth views

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from api.views import *
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('auth/register/', RegisterLibraryUser.as_view(), name='register-user'),
     path('books/', BookList.as_view(), name='book-list'),
+    path('books/<str:isbn>/', BookDetails.as_view(), name='book-details')
 ]
