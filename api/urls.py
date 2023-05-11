adc2a35bfd32e9f8628a2dc58295ca6571b97029
igornieb <igornieb@gmail.com>
2023-05-11 11:11:39 +0200

    added token and books endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 from api.views import *
 
 urlpatterns = [
-
-    ]
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('books/', BookList.as_view(), name='book-list'),
+]
